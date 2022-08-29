77b950e7d002e3394e2737a39a674828d8d41b9a
Yash Masani <yash.masani@gmail.com>
2022-08-28 20:20:40 -0400

    connect to db

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,13 @@ use mysql::*;
 use mysql::prelude::*;
 
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
+    let url = "mysql://root@localhost:3306";
+    let opts = OptsBuilder::new().user(Some("root")).db_name(Some("my_guitar_shop"));
+    let mut conn = Conn::new(opts)?;
+    let mut orders = conn.query_iter("SHOW COLUMNS from orders").unwrap();
+    for ords in orders.iter() {
+        ords.for_each(|o| println!("{:?}", o.unwrap()) )
+    }
+    Ok(())
 }
