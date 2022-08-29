3c31f78e6cc70bd39b286eaa2031444dde05760f
Yash Masani <yash.masani@gmail.com>
2022-08-29 09:10:03 -0400

    understanding the mysql wrapper

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,17 @@ fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
     let url = "mysql://root@localhost:3306";
     let opts = OptsBuilder::new().user(Some("root")).db_name(Some("my_guitar_shop"));
     let mut conn = Conn::new(opts)?;
-    let mut orders = conn.query_iter("SHOW COLUMNS from orders").unwrap();
+    let mut orders = conn.query_iter("SELECT * from orders LIMIT 5").unwrap();
+    
     for ords in orders.iter() {
-        ords.for_each(|o| println!("{:?}", o.unwrap()) )
+        ords.for_each(|o| {
+            for x in o.unwrap().unwrap().iter() {
+                print!("{}", x.as_sql(false));
+            }
+            print!("\n");
+        } );
+
     }
+    
     Ok(())
 }
